4a53f79a2bfee93ab98a25f1bd401dec3f0785c6
LtBackup <jamespratt6@gmail.com>
2018-02-01 11:19:53 -0600

    adds API call to OpenWeatherMap and saves wind speed to global windSpeed variable. Function name is getWindSpeed()

---
+++ assets/javascript/wind.js
@@ -0,0 +1,15 @@
+var appID = "&appid=cd2537336bd5be95e114a46e97d3ceef";
+var cityName = "Chicago";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + appID;
+var windSpeed;
+
+function getWindSpeed(){
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    windSpeed = response.wind.speed;
+});
+}
+
+console.log(getWindSpeed());
