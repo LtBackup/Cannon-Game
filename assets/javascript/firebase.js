70217b785b415b7ac88a7a47dbdbd16a8198b33f
Mina Slater <slater.mina@gmail.com>
2018-02-01 10:42:50 -0600

    starts integration code for Firebase

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD5Ev0o5EoYJiOBuHUVePoe22x-Nssfqd0",
+    authDomain: "cannon-game.firebaseapp.com",
+    databaseURL: "https://cannon-game.firebaseio.com",
+    projectId: "cannon-game",
+    storageBucket: "",
+    messagingSenderId: "500614387480"
+  };
+  firebase.initializeApp(config);
