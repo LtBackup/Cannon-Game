a14e9081b444535fa68d06a7443c3f416849ce87
Mina Slater <slater.mina@gmail.com>
2018-02-05 12:23:30 -0600

    merges remote master to incorporate cannon sprites

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,11 +16,6 @@ var cannonBallA,
 
 $(document).ready(function () {
   $(".overlay").addClass("opened");
-<<<<<<< HEAD
-=======
-  // adds click listener on join and start new game buttons in modal
-
->>>>>>> dd8e2ee37c207bbc807c4025ff2d5f9f6bcd6412
   $("#start-game").on("click", startGame);
   $("#join-game").on("click", function () {
     var newGameId = Number($("#game-id-field").val());
@@ -29,190 +24,7 @@ $(document).ready(function () {
   $("#fireButton").on("click", function () {
     fireCannon(window.gameInfo);
   });
-<<<<<<< HEAD
-
-    //create the canvas dimensions
-    var canvas = document.createElement("canvas");
-    var context = canvas.getContext("2d");
-    canvas.className = "canvas";
-    var element = document.getElementsByClassName("mainRow");
-
-    // canvas.width = 2000;
-    // canvas.height = 1500;
-    // // console.log(canvas);
-    // document.body.appendChild(canvas);
-    $(".mainRow").append(canvas);
-    //begin matter.js logic
-
-    // create a renderer
-    var render = Render.create({
-        element: document.body,
-        engine: engine,
-        canvas: canvas,
-        options: {
-            width: 1140,
-            height: 500,
-            showAngleIndicator: true,
-            wireframes: false,
-            background: './assets/images/canvasbg.jpg'
-        }
-    });
-
-    // define our categories (as bit fields, there are up to 32 available)
-    var defaultCategory = 0x0001,
-        playerOne = 0x0002,
-        playerTwo = 0x0004,
-        neutralPlatform = 0x0008;
-
-    var playerOneColor = '#C44D58',
-        playerTwoColor = '#4ECDC4';
-
-    //function createObjects(){
-    cannonA = Bodies.rectangle(90, 355, 75, 70, {
-        isStatic: true,
-        label: "cannonA",
-        collisionFilter: {
-            category: playerOne
-        },
-        render: {
-            fillStyle: playerOneColor
-        }
-    });
-
-    launchPlatformA = Bodies.rectangle(90, 400, 60, 60, {
-        isStatic: true,
-        label: "launchPlatform",
-        friction: 1,
-        collisionFilter: {
-            category: neutralPlatform
-        }
-    });
-
-    cannonB = Bodies.rectangle(1000, 355, 75, 70, {
-        isStatic: true,
-        label: "cannonB",
-        collisionFilter: {
-            category: playerTwo
-        },
-        render: {
-            fillStyle: playerTwoColor
-        }
-    });
-
-    launchPlatformB = Bodies.rectangle(1000, 400, 60, 60, {
-        isStatic: true,
-        label: "launchPlatform",
-        friction: 1,
-        collisionFilter: {
-            category: neutralPlatform
-        },
-    });
-
-    cannonBallA = Bodies.circle(90, 260, 16, {
-        label: "cannonBallA",
-        friction: 1,
-        restitution: 0,
-        mass: 1.9444530819999999,
-        collisionFilter: {
-            mask: defaultCategory | playerTwo | neutralPlatform
-        },
-        render: {
-            fillStyle: playerOneColor
-        }
-    });
-    cannonBallAOrigin = { x: cannonBallA.position.x, y: cannonBallA.position.y };
-
-    cannonBallB = Bodies.circle(1000, 250, 16, {
-        label: "cannonBallB",
-        friction: 1,
-        restitution: 0,
-        mass: 1.9444530819999999,
-        collisionFilter: {
-            mask: defaultCategory | playerOne | neutralPlatform
-        },
-        render: {
-            fillStyle: playerOneColor
-        }
-    });
-    cannonBallBOrigin = { x: cannonBallB.position.x, y: cannonBallB.position.y };
-
-    ground = Bodies.rectangle(570, 442, 1140, 100, {
-        isStatic: true,
-        label: "ground",
-        friction: 1,
-        collisionFilter: {
-            category: defaultCategory
-        }
-    });
-
-    // add all of the bodies to the world
-    World.add(engine.world, [cannonA, cannonB, launchPlatformA, launchPlatformB, cannonBallA, cannonBallB, ground]);
-    //}
-
-
-    // run the engine
-    Engine.run(engine);
-
-    // run the renderer
-    Render.run(render);
-
-    // Checks to see if the active collision involves the cannonball and stops it from spinning if so
-
-    Events.on(engine, "collisionActive", function (event) {
-        var pairs = event.pairs;
-        for (var i = 0; i < pairs.length; i++) {
-            var pair = pairs[i];
-            //stops rolling motion if on launch platform
-            if ((pair.bodyA.label === "cannonBallA" && pair.bodyB.label === "launchPlatform") || (pair.bodyA.label === "launchPlatform" && pair.bodyB.label === "cannonBallA")) {
-                Body.setVelocity(cannonBallA, { x: 0, y: 0 });
-                Body.setAngularVelocity(cannonBallA, 0);
-            }
-            if ((pair.bodyA.label === "cannonBallB" && pair.bodyB.label === "launchPlatform") || (pair.bodyA.label === "launchPlatform" && pair.bodyB.label === "cannonBallB")) {
-                Body.setVelocity(cannonBallB, { x: 0, y: 0 });
-                Body.setAngularVelocity(cannonBallB, 0);
-            }
-            //checks for impact with enemy cannons and ground
-            if ((pair.bodyA.label === "cannonBallA" && pair.bodyB.label === "cannonB") || (pair.bodyB.label === "cannonBallA" && pair.bodyA.label === "cannonB")) {
-                //TODO trigger explosion
-                resetBallA();
-                console.log("Player 1 wins");
-            }
-            if ((pair.bodyA.label === "cannonBallB" && pair.bodyB.label === "cannonA") || (pair.bodyB.label === "cannonBallB" && pair.bodyA.label === "cannonA")) {
-                //TODO trigger explosion
-                resetBallB();
-                console.log("Player 2 wins");
-            }
-            if ((pair.bodyA.label === "cannonBallA" && pair.bodyB.label === "ground") || (pair.bodyB.label === "cannonBallA" && pair.bodyA.label === "ground")) {
-                resetBallA();
-                console.log("miss");
-            }
-            if ((pair.bodyA.label === "cannonBallB" && pair.bodyB.label === "ground") || (pair.bodyB.label === "cannonBallB" && pair.bodyA.label === "ground")) {
-                resetBallB();
-                console.log("miss");
-            }
-        }
-    });
-
-    //-Player 1 controls________________________________
-    var angle; var power;
-    var pRange = document.getElementById("pRange");
-    var aRange = document.getElementById("aRange");
-    var p_output = document.getElementById("p-out");
-    var a_output = document.getElementById("a-out");
-    p_output.innerHTML = pRange.value;
-    a_output.innerHTML = aRange.value;
-
-    pRange.oninput = function () {
-        power = this.value;
-        p_output.innerHTML = power;
-        console.log("Power: ", typeof power);
-=======
-  $("#fireButton2").on("click", function () {
-    fireCannon(window.gameInfo);
-  });
-
-  // TODO: add firebase listeners on opponent player's data change
-
+  
   //create the canvas dimensions
   var canvas = document.createElement("canvas");
   var context = canvas.getContext("2d");
@@ -237,7 +49,6 @@ $(document).ready(function () {
       showAngleIndicator: true,
       wireframes: false,
       background: './assets/images/canvasbg.jpg'
->>>>>>> dd8e2ee37c207bbc807c4025ff2d5f9f6bcd6412
     }
   });
 
