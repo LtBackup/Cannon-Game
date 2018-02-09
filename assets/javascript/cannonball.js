39daa2ca48fcefbefbc0fe5cb1dba77715f5fdf6
Mina Slater <slater.mina@gmail.com>
2018-02-09 10:57:29 -0600

    adds the cannon fire sounds that got lost in the conflict resolution

--- assets/javascript/cannonball.js
+++ assets/javascript/cannonball.js
@@ -22,6 +22,8 @@ var cannonballBot = (function() {
   }
 
   function fireCannon(gameInfo) {
+    audio.cannonSound.load();
+    audio.cannonSound.play();
     var currentPlayer = gameInfo.player;
     var gameId = gameInfo.gameId;
     var angleInput;
@@ -40,6 +42,8 @@ var cannonballBot = (function() {
   }
 
   function launchCannonBall(angle, power) {
+    audio.cannonSound.load();
+    audio.cannonSound.play();
     var dampener = .003;
     var launchVector = Matter.Vector.create(Math.cos(toRadians(angle)) * (power * dampener), -Math.sin(toRadians(angle)) * (power * dampener));
     var launchVector2 = Matter.Vector.create(-Math.cos(toRadians(angle)) * (power * dampener), -Math.sin(toRadians(angle)) * (power * dampener));
